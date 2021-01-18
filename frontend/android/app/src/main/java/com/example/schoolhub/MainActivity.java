069c282a7dd95bb4a7bce1b494536663e7a4cf97
Syed Saad Ullah Shah <shahsaadullah11@gmail.com>
2021-01-18 20:21:41 +0500

    first commit

---
+++ frontend/android/app/src/main/java/com/example/schoolhub/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.schoolhub;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Intent it = new Intent( getApplicationContext() ,SchoolDetails.class);
+//        startActivity(it);
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(getApplicationContext(), UserSignIn.class);
+                startActivity(intent);
+            }
+        }, 2000);//timer set for 2 seconds
+   }
+}
