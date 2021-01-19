43b66ec0ea8f05ab97edc66443e7f80c4d364b74
osama2z <mosama4u@gmail.com>
2021-01-19 16:22:31 +0500

    adding new react app

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -1,17 +1,22 @@
 
 import SignIn from './signin';
 import SignUp from './signup';
+import Schoolprofile from './SchoolProfile';
+import SchoolFaculty from './SchoolFaculty';
+import SchoolFee from './SchoolFee';
+import SchoolAcad from './SchoolAcad';
 import './App.css';
+import SideBar from './SideBar';
+import Home from './home'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-      <Router>
-        <Switch>
-        <Route exact path='/' component={SignUp}/> 
-        <Route exact path='/signin' component={SignIn}/> 
-        </Switch>
-      </Router>
+    <Home/>
+      // <Router className='main'>
+      //   <Route exact path='/' component={SignUp}/> 
+      //   <Route exact path='/signin' component={SignIn}/>  
+      // </Router>
   );
 }
 
